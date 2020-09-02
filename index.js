92008a83c4bdc83fb2c4b29c33456d8b8c15288f
Cristian Dominguez <57419590+itscrisu@users.noreply.github.com>
2020-09-02 03:09:46 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-/* This was one of many exercises I did learning JavaScript in freeCodeCamp that I found really interesting. This one's called: "Counting Cards", wich basically asks us to make a function that could receive a 'card' parameter, wich can be a string or a number, and increment or decrement the global 'count' variable according to the card's value. The function will then return a string with the current count and the string Bet if the count is positive, or Hold if the count is zero or negative. The current count and the player's decision (Bet or Hold) should be separated by a single space.    */
-
-
 var count = 0;
 
 function cc(card){
@@ -21,11 +18,7 @@ function cc(card){
       break;
   }
 
-var holdBet = "Hold"
-if (count < 0){
-  holdBet = "Bet"
-}
-return count + (count > 0 ? " Bet" : " Hold");
+  return count + (count > 0 ? " Bet" : " Hold");
 }
 
 
